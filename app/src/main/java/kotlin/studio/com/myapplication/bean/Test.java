a4db9a2c45cae906a9232fa6132482b5ba5b07cc
15296711325@163.com <hrj397125>
2018-07-23 21:02:04 +0800

    test

---
+++ app/src/main/java/kotlin/studio/com/myapplication/bean/Test.java
@@ -0,0 +1,11 @@
+package kotlin.studio.com.myapplication.bean;
+
+/**
+ * Description:
+ * Copyright  : Copyright (c) 2016
+ * Company    : Android
+ * Author     : 关羽
+ * Date       : 2018/7/23 21:01
+ */
+public class Test {
+}
